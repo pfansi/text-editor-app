133064ed398fcdf8d0f3c0a40a41d5f242ffdf6c
pfansiW <fancyfreelance76@gmail.com>
2022-04-12 03:51:00 +0100

    modified the function putDb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,26 +15,15 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 // export const putDb = async (content) => console.error("putDb not implemented");
 
-export const putDb = async (id, content) => {
+export const putDb = async (content) => {
   console.log("PUT to the database");
   const jateDb = await openDB("jate", 1);
   const tx = jateDb.transaction("jate", "readwrite");
   const store = tx.objectStore("jate");
-  const request = store.put({ id: id, jate: content });
+  const request = store.put({ id: 1, jate: content });
   const result = await request;
   console.log("🚀 - data saved to the database", result);
 };
-
-// export const putDb = async (content) => {
-//   try {
-//     const editorDb = await openDB('jate', 1);
-//     const tx = editorDb.transaction('jate', 'readwrite');
-//     const store = tx.objectStore('jate');
-//     await store.put({ id: 1, value: content });
-//   } catch (error) {
-//     console.error('putDb not implemented');
-//   }
-// };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
